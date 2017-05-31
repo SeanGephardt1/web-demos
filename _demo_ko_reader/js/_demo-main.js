75307a79efbf37de23f5a4c32edc34e757e9b970
Sean Gephardt <v-segeph@microsoft.com>
2017-05-31 12:17:56 -0700

    baseline checkin

--- _demo_ko_reader/js/_demo-main.js
+++ _demo_ko_reader/js/_demo-main.js
@@ -2,27 +2,27 @@
 ( function ()
 {
     var _debug_flag = false;
-    var _demo_name = "Knockout.js Demo Template";
+    var _demo_name = "Knockout.js Reader Demo";
     try
     {
-        console.info( "BEGIN", _demo_name );
+        console.info( "BEGIN", _demo_name, new Date().toJSON() );
         window.document.addEventListener( "DOMContentLoaded", function ( ev )
         {   //	console.log( "DOMContentLoaded." );
-            var _main_vm = new MainViewModel( _demo_name, _debug_flag );
+            var _main_vm = new ReaderViewModel( _demo_name, _debug_flag );
             ko.applyBindings( _main_vm );
             return;
         } );
     }
     catch ( ex )
     {
-        console.error( "Exception", _demo_name );
+        console.error( "Exception::", _demo_name );
         console.error( ex.number, ":", ex.name, ":", ex.message );
         console.error( "stack::", ex.stack );
         return;
     }
     finally
     {
-        console.info( "END", _demo_name );
+        console.info( "END", _demo_name, new Date().toJSON() );
     }
     return;
 } )();
