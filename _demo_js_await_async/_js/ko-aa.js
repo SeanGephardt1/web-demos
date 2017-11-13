eae672e3812d6cf3c21a772e1134fa6cd49d22a0
Sean Gephardt <v-segeph@microsoft.com>
2017-11-13 09:57:35 -0800

    added KO.JS

--- _demo_js_await_async/_js/ko-aa.js
+++ _demo_js_await_async/_js/ko-aa.js
@@ -9,7 +9,7 @@
 		{
 			setTimeout( () =>
 			{
-				resolve( x * 2 );
+				resolve( x * 3 );
 			}, 2000);
 		});
 	}
@@ -18,30 +18,30 @@
 	{
 		const a = await this.Double( 10 );
 		console.debug( "await:a:", a );
-		this.Output( a );
+		this.Output("a: " +  a );
 
 		const b = await this.Double( 20 );
 		console.debug( "await:b:", b );
-		this.Output( b );
+		this.Output("b: " +  b );
 
 		const c = await this.Double( 30 );
 		console.debug( "await:c:", c );
-		this.Output( c );
+		this.Output( "c: " + c );
+
+		window.setTimeout(() =>
+		{
+			 console.log("F");
+			 return;
+		}, 5000);
 
 		return x + a + b + c;
 	}
-
-	//this.addAsync = function( inputNumber )
-	//{
-	//	console.log( sum );
-	//	this.AddAsync( inputNumber );
-	//});
 
 	this.Init = ko.computed( function ()
 	{	//	console.debug("this.init");
 		this.AddAsync( 10 ).then( ( sum ) =>
 		{	//	console.debug( "then.sum", sum );
-			this.Output( sum );
+			this.Output( "sum = " + sum );
 			return;
 		} );
 
