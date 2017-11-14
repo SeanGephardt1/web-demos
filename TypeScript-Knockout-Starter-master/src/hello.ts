1fbe72e2f5a6078f4825f7ec488722dc3ba37e46
Sean Gephardt <v-segeph@microsoft.com>
2017-11-14 01:08:37 -0800

    update to working version in Visual Studio 2017

--- TypeScript-Knockout-Starter-master/src/hello.ts
+++ TypeScript-Knockout-Starter-master/src/hello.ts
@@ -1,13 +1,30 @@
+"use strict";
 import * as ko from "knockout";
 
-class HelloViewModel {
+class HelloViewModel
+{
+    APPDEBUG: KnockoutObservable<boolean>
     language: KnockoutObservable<string>
     framework: KnockoutObservable<string>
+    userConfig: KnockoutObservableArray<any>
 
-    constructor(language: string, framework: string) {
-        this.language = ko.observable(language);
-        this.framework = ko.observable(framework);
+    constructor( _debug: boolean, language: string, framework: string, config_data: any )
+    {
+        var _self = this;
+        this.APPDEBUG = ko.observable( _debug );
+        this.language = ko.observable( language );
+        this.framework = ko.observable( framework );
+
+        this.userConfig = ko.observableArray( [config_data] );
     }
+    return;
 }
 
-ko.applyBindings(new HelloViewModel("TypeScript", "Knockout"));
+let _app_config = {
+    AppTitle: "Azure Prototype V2 Template Project",
+    Copyright: "2018 Microsoft Corporation",
+    Version: "2.0.1",
+    Theme: "Dark"
+};
+
+ko.applyBindings( new HelloViewModel( true,"TypeScript", "Knockout", _app_config ) );
