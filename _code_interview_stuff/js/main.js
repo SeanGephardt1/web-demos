46ac8e5d0af046806db3bf551a93919815a0c825
Sean Gephardt <seangephardt@live.com>
2020-03-17 12:35:32 -0700

    Coding Questions WIP

--- _code_interview_stuff/js/main.js
+++ _code_interview_stuff/js/main.js
@@ -1,4 +1,7 @@
 ﻿"use strict";
+import ProblemOne from './problem-1.js';
+import ProblemTwo from './problem-2.js';
+import ProblemThree from './problem-3.js';
 
 ( function ()
 {
@@ -8,28 +11,18 @@
 	{
 		console.info( "BEGIN", _demo_name );
 		window.document.addEventListener( "DOMContentLoaded", function ( ev )
-		{
-			console.log( "DOMContentLoaded" );
-			console.debug( "FIND THE NEXT LOWEST INTEGER ABOVE 0" );
+		{	//	console.log( "DOMContentLoaded" );
 
-			const _array_1 = [1, 3, 6, 4, 1, 2, 6, 4, 3]; // 5
-			const _array_2 = [1, 2, 3]; // 4
-			const _array_3 = [-1, -3]; // 1
-			const _array_4 = [-99, -1, -5, -1, -3, 6, 19, 1, 9, 31,4]; // 5
+			const _p1 = new ProblemOne();
+			_p1.DoProblems();
 
-			let _answer;
+			const _p2 = new ProblemTwo();
+			_p2.DoProblems();
 
-			_answer = solution( _array_1 ); // 5
-			console.debug( "_answer 1: expected answer: 5: \t", _answer[0],  _answer[1] );
+			//const _p3 = new ProblemThree();
+			//_p3.DoProblems();
 
-			_answer = solution( _array_2 ); // 4
-			console.debug( "_answer 2: expected answer: 4: \t", _answer[0],  _answer[1] );
 
-			_answer = solution( _array_3 ); // 1
-			console.debug( "_answer 3: expected answer: 1: \t",_answer[0],  _answer[1] );
-
-			_answer = solution( _array_4 ); // 2
-			console.debug( "_answer_4: expected answer: 3: \t", _answer[0],  _answer[1] );
 			return;
 		} );
 	}
@@ -47,46 +40,3 @@
 	return;
 } )();
 
-function solution( A )
-{
-	// write your code in JavaScript (Node.js 8.9.4)
-	let _return_value = undefined;
-	//	console.debug( "A", A.length, A );
-
-	let _filtered = A.filter( val => val > 0 ).sort( ( a, b ) =>
-	{
-		if ( a > b )
-		{ return 1; }
-
-		if ( a < b )
-		{ return -1; }
-
-		if ( a === b )
-		{ return 0; }
-	} );
-	//	console.debug( "_filtered", _filtered.length, _filtered );
-
-	let _arr = Array.from( new Set( _filtered ) );
-	//	console.debug( "_arr", _arr );
-
-	for ( let i = 0; i < _arr.length; i++ )
-	{
-		//	console.debug( "for:", _arr[i], _arr[i + 1] );
-		if (_arr[i] +1 === _arr[i + 1])
-		{
-			continue;
-		}
-		else if (_arr[i] +1 !== _arr[i + 1] )
-		{
-			//	console.debug( "breaking");
-			_return_value = ( _arr[i] + 1 );
-			break;
-		}
-	}
-
-	if ( _return_value === undefined )
-	{	
-		_return_value = _arr.length + 1;
-	}
-	return [A,_return_value];
-}
---
+++ _code_interview_stuff/js/problem-3.js
@@ -0,0 +1,72 @@
+﻿export default class ProblemThree
+{
+	constructor()
+	{
+		console.debug( "FIND THE NEXT LOWEST INTEGER ABOVE 0" );
+		this._array_1 = [1, 3, 6, 4, 1, 2, 6, 4, 3]; // 5
+		this._array_2 = [1, 2, 3]; // 4
+		this._array_3 = [-1, -3]; // 1
+		this._array_4 = [-99, -1, -5, -1, -3, 6, 19, 1, 9, 31,4]; // 5
+		this.Answer;
+		return;
+	};
+	Solution( A )
+	{
+		// write your code in JavaScript (Node.js 8.9.4)
+		let _return_value = undefined;
+		//	console.debug( "A", A.length, A );
+
+		let _filtered = A.filter( val => val > 0 ).sort( ( a, b ) =>
+		{
+			if ( a > b )
+			{ return 1; }
+
+			if ( a < b )
+			{ return -1; }
+
+			if ( a === b )
+			{ return 0; }
+		} );
+		//	console.debug( "_filtered", _filtered.length, _filtered );
+
+		let _arr = Array.from( new Set( _filtered ) );
+		//	console.debug( "_arr", _arr );
+
+		for ( let i = 0; i < _arr.length; i++ )
+		{
+			//	console.debug( "for:", _arr[i], _arr[i + 1] );
+			if (_arr[i] +1 === _arr[i + 1])
+			{
+				continue;
+			}
+			else if (_arr[i] +1 !== _arr[i + 1] )
+			{
+				//	console.debug( "breaking");
+				_return_value = ( _arr[i] + 1 );
+				break;
+			}
+		}
+
+		if ( _return_value === undefined )
+		{	
+			_return_value = _arr.length + 1;
+		}
+		return [A,_return_value];
+	};
+	DoProblems()
+	{
+		this.Answer = this.Solution( this._array_1 ); // 5
+		console.debug( "this.Answer 1: expected answer: 5: \t", this.Answer[0],  this.Answer[1] );
+
+		this.Answer = this.Solution( this._array_2 ); // 4
+		console.debug( "this.Answer 2: expected answer: 4: \t", this.Answer[0],  this.Answer[1] );
+
+		this.Answer = this.Solution( this._array_3 ); // 1
+		console.debug( "this.Answer 3: expected answer: 1: \t",this.Answer[0],  this.Answer[1] );
+
+		this.Answer = this.Solution( this._array_4 ); // 2
+		console.debug( "this.Answer: expected answer: 3: \t", this.Answer[0], this.Answer[1] );
+
+		return;
+	}
+}
