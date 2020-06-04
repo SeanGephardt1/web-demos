8beff259e5e98dec565fd1f15d6070529f03636b
Sean Gephardt <seangephardt@live.com>
2020-06-03 21:11:02 -0700

    Coding Questions, Sample 3

--- _code_interview_stuff/js/problem-3.js
+++ _code_interview_stuff/js/problem-3.js
@@ -1,10 +1,10 @@
 ﻿// Given a matrix of numbers, if an element is 0, then set it's entire row and column to 0.
-
+/*
 //Input:
 //[
-//  [1,1,0],
-//  [1,1,1],
-//  [1,1,1]
+//  [1,1,0], // row 0 - true
+//  [1,1,1], // row 1 - false
+//  [1,1,1]  // row 0 - false
 //]
 //Output:
 //[
@@ -12,26 +12,42 @@
 //  [1,1,0],
 //  [1,1,0]
 //]
-//Input:
-//[
-//  [1,1,0], // row 0 - true
-//  [1,1,1], // row 1 - false
-//  [0,1,1]  // row 2 - true
-//]
-//Output:
-//[
-//  [0,0,0],
-//  [0,1,0],
-//  [0,0,0]
-//]
+*/
+
+/*
+Input:
+[
+  [1,1,0], // row 0 - true
+  [1,1,1], // row 1 - false
+  [0,1,1]  // row 2 - true
+]
+Output:
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+*/
+
+/*
+Input:
+[
+  [1,1,1], // row 0 - true
+  [1,0,1], // row 1 - false
+  [1,1,1]  // row 2 - true
+]
+Output:
+[
+  [1,0,1],
+  [0,0,0],
+  [1,0,1]
+]
+*/
 
 export default class ProblemThree
 {
 	constructor()
-	{
-		console.debug( "Change the values in a matrix, based on values in the first array in a jagged array" );
-		this.Answer;
-
+	{	console.debug( "Change the values in a matrix, based on values in the first array in a jagged array" );
 		this.ArrayOne = [
 			[1, 1, 0],
 			[1, 1, 1],
@@ -42,38 +58,75 @@ export default class ProblemThree
 			[1, 1, 1],
 			[0, 1, 1]
 		];
-
+		this.ArrayThree = [
+			[1, 1, 1],
+			[1, 0, 1],
+			[1, 1, 1]
+		];
 		return;
 	};
-	Solution( A )
+	Solution( data )
 	{
+		let inputArray = JSON.parse( JSON.stringify( data ) );
+		let _data = [];
+		let _col_set = [];
 
-		for (let i = 0; i< _arr1.length; i++)
-		{
-		  let _row = [];
-		  for (let j = 0; j< _arr1[i].length; j++)
-		  {
- 
-		    let _col = _arr1[i][j];
-		    if (_col === 0)
-		    {
-		      //
-		      _row.push(true);  
-		    }
-		     else if (_col === 1)
-		     {
-		       _row.push(false);
-		     }
-		  }
-		  _data.push(_row);
+		// FIND THE ROWS THAT CONTAIN ZEROS
+		for ( let i = 0; i < inputArray.length; i++ )
+		{	//	console.debug( inputArray[i] );
+			_col_set[i] = 1;
+			let _hasZero = inputArray[i].indexOf(0);
+			//	console.debug( i, "_hasZero", _hasZero );
+
+			if ( _hasZero !== -1 )
+			{
+				_data[i] = { found: true, cols: [...inputArray[i]] };
+			}
+			else 	if ( _hasZero === -1 )
+			{
+				_data[i] =  { found: false, cols: [...inputArray[i]] };
+			}
+		}
+		//	console.debug( "_data", _data, _col_set );
+
+		// COLLAPSE COLUMN MAPPING
+		for ( let i = 0; i < _data.length; i++ )
+		{	//	console.debug( "data", i, _data[i].cols );
+			for ( let k = 0; k < _data[i].cols.length; k++ )
+			{
+				//	console.debug( i, k, "data", _data[i].cols[k], _col_set[i] );
+				if (_col_set[k] === 1 && _data[i].cols[k] === 0)
+				{
+					_col_set[k] = 0;
+				}
+			}
+
+			//	WORKING???
+			if ( _data[i].found === true )
+			{
+				for ( let j = 0; j < inputArray[i].length; j++ )
+				{
+					inputArray[i][j] = 0;
+				}
+			}
+			else if ( _data[i].found === false )
+			{
+				inputArray[i] = _col_set;
+			}
 		}
 
-		_ret_arr.sort();
-		return _ret_arr;
+		return inputArray;
 	};
 	DoProblems()
 	{
-		this.Solution(); 
+		const returnedValue1 = this.Solution( this.ArrayOne ); 
+		console.debug( "One: input:", this.ArrayOne, "return value:", returnedValue1 );
+
+		const returnedValue2 = this.Solution( this.ArrayTwo ); 
+		console.debug( "Two: input:", this.ArrayTwo, "return value:", returnedValue2 );
+
+		const returnedValue3 = this.Solution( this.ArrayThree ); 
+		console.debug( "Three: input:", this.ArrayThree, "return value:", returnedValue3 );
 		return;
 	}
 }
